a2fd18343b0ef2e9a84677eb2ae3c3218fc61bfc
jubair <jubairislamasif@gmail.com>
2024-08-12 18:37:51 +0600

    initial commit

---
+++ assets/js/custom.js
@@ -0,0 +1,46 @@
+// Menu
+function toggleMenu() {
+  const menu = document.getElementById("mobile-menu");
+  menu.classList.toggle("hidden");
+}
+
+// Video
+document.addEventListener("DOMContentLoaded", function () {
+  const playBtn = document.querySelector(".play-btn");
+  const videoContainer = document.getElementById("videoContainer");
+  const closeBtn = document.getElementById("closeBtn");
+  const video = document.getElementById("video");
+
+  if (playBtn && videoContainer && closeBtn && video) {
+    playBtn.addEventListener("click", function () {
+      videoContainer.classList.remove("hidden");
+      video.play();
+    });
+
+    closeBtn.addEventListener("click", function (e) {
+      e.preventDefault();
+      videoContainer.classList.add("hidden");
+      video.pause();
+      video.currentTime = 0;
+    });
+  }
+});
+
+// FAQ
+function toggleAccordion(index) {
+  var accordions = document.getElementsByClassName("according");
+  for (let i = 0; i < accordions.length; i++) {
+    var content = accordions[i].getElementsByClassName("according-content")[0];
+    var svg = accordions[i].querySelector(".according-header img");
+    if (i === index) {
+      content.style.display =
+        content.style.display === "block" ? "none" : "block";
+      svg.classList.toggle("rotate-90");
+      accordions[i].classList.add("bg-faq-card-item");
+    } else {
+      content.style.display = "none";
+      svg.classList.remove("rotate-90");
+    }
+  }
+}
+toggleAccordion(0);
