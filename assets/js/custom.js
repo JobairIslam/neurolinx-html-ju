c319a24849c94803953e1183a52d85055f3a0421
jubair <jubairislamasif@gmail.com>
2024-09-02 18:30:31 +0600

    ADDED : mega menu helf

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,22 +1,54 @@
 // Menu
+// function toggleMenu() {
+//   const menu = document.getElementById("mobile-menu");
+//   menu.classList.toggle("hidden");
+// }
+//mega menu for phone
+document.addEventListener("DOMContentLoaded", () => {
+  const resourcesLink = document.getElementById("resources-link");
+  const companyLink = document.getElementById("company-link");
+
+  resourcesLink.addEventListener("click", (event) => {
+    event.preventDefault();
+    toggleMenu("resources-menu", "resources-icon");
+  });
+
+  companyLink.addEventListener("click", (event) => {
+    event.preventDefault();
+    toggleMenu("company-menu", "company-icon");
+  });
+});
+
+function toggleMenu(menuId, iconId) {
+  const menu = document.getElementById(menuId);
+  const icon = document.getElementById(iconId);
+
+  // Toggle the menu visibility
+  menu.classList.toggle("show");
+
+  // Rotate the icon
+  if (menu.classList.contains("show")) {
+    icon.style.transform = "rotate(180deg)";
+  } else {
+    icon.style.transform = "rotate(0deg)";
+  }
+}
+
 function toggleMenu() {
   const menu = document.getElementById("mobile-menu");
   menu.classList.toggle("hidden");
 }
-
 // Video
 document.addEventListener("DOMContentLoaded", function () {
   const playBtn = document.querySelector(".play-btn");
   const videoContainer = document.getElementById("videoContainer");
   const closeBtn = document.getElementById("closeBtn");
   const video = document.getElementById("video");
-
   if (playBtn && videoContainer && closeBtn && video) {
     playBtn.addEventListener("click", function () {
       videoContainer.classList.remove("hidden");
       video.play();
     });
-
     closeBtn.addEventListener("click", function (e) {
       e.preventDefault();
       videoContainer.classList.add("hidden");
@@ -25,7 +57,6 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
-
 // FAQ
 function toggleAccordion(index) {
   const accordingItems = document.querySelectorAll(".according");
